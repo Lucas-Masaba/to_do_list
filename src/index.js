eeb3c6408915164ccdb1309c81e5b07766481347
Khusiima Luke Masaba <masabaluke@gmail.com>
2021-12-01 06:11:34 +0300

    add content to all src files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
+/* eslint-disable no-unused-vars, no-undef, no-return-assign */
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const tasksToDo = [
+  {
+    id: '0',
+    description: 'wash the dishes',
+    completed: 'bool',
+  },
+  {
+    id: '1',
+    description: 'complete To Do list project',
+    completed: 'bool',
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const taskCollection = () => {
+  const taskUL = document.getElementById('tasks_id');
 
-  return element;
-}
-
-document.body.appendChild(component());
+  tasksToDo.forEach((task) => {
+    taskUL.insertAdjacentHTML('beforeend', ` <li class="task_list">
+                        <input type="checkbox" class="checkbox_style" id="${task.id}"  name="${task.description}">
+                        <label for="${task.description}">${task.description}</label><hr>
+                       </li>`);
+  });
+};
+document.addEventListener('DOMContentLoaded', () => {
+  taskCollection();
+});
