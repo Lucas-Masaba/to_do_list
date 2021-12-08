772821f1328accfced6911e23331aa7a41db04c3
ShadyShawkat <shadyshawkat91@gmail.com>
2021-12-09 00:10:05 +0200

    Create test for removing an item

--- src/handlers.test.js
+++ src/handlers.test.js
@@ -1,30 +1,20 @@
-jest.mock('./data');
+jest.mock('./handlers');
 import Handlers from './handlers'
-// jest.mock('./handlers');
 
 describe('Test for remove item', () => {
   test('item is removed from the array', () => {
-    document.body.innerHTML = '';
-    const event = {
-      target: {
-        className: 'remove-item',
-        getAttribute(string) {
-          return 0;
-        }
-      }
-    };
-    // const allTasks = [
-    //   { 
-    //     description: 'Task 1', 
-    //     index: 0,
-    //     completed: false,
-    //   },
-    //   { 
-    //     description: 'Task 2', 
-    //     index: 1,
-    //     completed: true,
-    //   },
-    // ];
-    expect(Handlers.removeTask(event)).toHaveLength(1);
+    const allTasks = [
+      { 
+        description: 'Task 1', 
+        index: 0,
+        completed: false,
+      },
+      { 
+        description: 'Task 2', 
+        index: 1,
+        completed: true,
+      },
+    ];
+    expect(Handlers.removeTask(0, allTasks)).toHaveLength(1);
   })
 })
