ac2ededf18f3cfca3a68a0306f471a7b72dbc38c
Maclenn77 <maclenn77@gmail.com>
2021-12-09 13:28:09 -0600

    Clean code moving scope

--- src/handlers.test.js
+++ src/handlers.test.js
@@ -3,40 +3,33 @@ import Data from './data.js';
 
 jest.mock('./handlers');
 
+beforeEach(() => {
+  document.body.innerHTML = `
+  <ul>
+    <button id="clear-all">Clear all</button>
+  </ul>`; 
+})
+
+const allTasks = [
+  {
+    description: 'Task 1',
+    index: 0,
+    completed: false,
+  },
+  {
+    description: 'Task 2',
+    index: 1,
+    completed: true,
+  },
+];
+
 describe('Test for remove item', () => {
   test('item is removed from the array', () => {
-    const allTasks = [
-      {
-        description: 'Task 1',
-        index: 0,
-        completed: false,
-      },
-      {
-        description: 'Task 2',
-        index: 1,
-        completed: true,
-      },
-    ];
+
     expect(Handlers.removeTask(0, allTasks)).toHaveLength(1);
   });
 
   test('item is removed from the array in the DOM', () => {
-    document.body.innerHTML = `
-    <ul>
-      <button id="clear-all">Clear all</button>
-    </ul>`;
-    const allTasks = [
-      {
-        description: 'Task 1',
-        index: 0,
-        completed: false,
-      },
-      {
-        description: 'Task 2',
-        index: 1,
-        completed: true,
-      },
-    ];
     Handlers.renderList(allTasks);
     Handlers.removeTask(0, allTasks);
     const lis = document.querySelectorAll('.task-item');
@@ -46,45 +39,29 @@ describe('Test for remove item', () => {
 
 describe('test add item', () => {
   test('test add item', () => {
-    document.body.innerHTML = `
-    <ul>
-      <button id="clear-all">Clear all</button>
-    </ul>`;
     Handlers.handleAddTask('description', false);
     const lis = document.querySelectorAll('.task-item');
     expect([...lis]).toHaveLength(1);
   });
 });
 
-describe('test update description of item', () => {
+describe('test edit description of item', () => {
   beforeEach(() => {
-    document.body.innerHTML = `
-    <ul>
-      <button id="clear-all">Clear all</button>
-    </ul>`;    
-    const allTasks = [
-      {
-        description: 'Task 1',
-        index: 0,
-        completed: false,
-      },
-      {
-        description: 'Task 2',
-        index: 1,
-        completed: true,
-      },
-    ];
     Data.resetData(allTasks);
     Handlers.renderList(allTasks);
     Handlers.handleUpdate(0,'new task description')
   })
   
-  test('test update description', () => {
+  test('test edit description', () => {
     expect(Data.allTasks[0].description).toBe('new task description');
   });
 
-  test('test update description in DOM', () => {
+  test('test edit description in DOM', () => {
     const descriptions = document.querySelectorAll('.description');
     expect([...descriptions][0].textContent).toBe('new task description');
   });
 });
+
+// describe('test update completed task', () => {
+//   test('')
+// })
